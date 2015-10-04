bf08bf630a953c999286f05b83e03be2d361d5ce
nikintharan <nikin.tharan@gmail.com>
2015-10-03 22:31:22 -0400

    Update indico.js
    Automatic gathering of post data on mouse scroll

--- js/indico.js
+++ js/indico.js
@@ -1,12 +1,32 @@
+
+$("body").bind("click mousedown", function() {
+   
 $(window).ready(function() {
 	var posts = $("p").map(function() {
 		return $(this)[0].innerText;
 	});
 	console.log(posts);
+}); 
 });
 
 var statuses = ["Omg", "Becky", "No way"];
 var apikey = "368f9df404609aed0e36f71d89e5b992";
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '495580443948548',
+      xfbml      : true,
+      version    : 'v2.4'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
 
 $.ajax({
     url: "https://apiv2.indico.io/sentiment/batch?key=" + apikey,
