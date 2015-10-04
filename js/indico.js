a3eae7343ba6cd79ea934ac0cb2e93cc62ee0667
Ming Luo <mcluo1994@gmail.com>
2015-10-03 21:26:56 -0400

    console logged some status posts

--- js/indico.js
+++ js/indico.js
@@ -1,3 +1,10 @@
+$(window).ready(function() {
+	var posts = $("p").map(function() {
+		return $(this)[0].innerText;
+	});
+	console.log(posts);
+});
+
 var statuses = ["Omg", "Becky", "No way"];
 var apikey = "368f9df404609aed0e36f71d89e5b992";
 
@@ -11,6 +18,3 @@ $.ajax({
         console.log(err); //indent
     }
 });
-
-console.log(document.getElementsByTagName("p"));
-// should find child of .fbTimelineCapsule, or ._5
