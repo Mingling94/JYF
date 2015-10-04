b0a5073a928396491b47f593ceeb8bede4f78f63
lunete <claud@openmailbox.org>
2015-10-03 21:57:04 -0400

    teaching jquery

---
+++ js/linker.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    // $('button').on('click', function() {
+    //     $(this).animate({left: '200px'}, slow);
+    //     alert("it works!"));
+    // });
+    $('div.bottom-row').on('click', 'button.basic', function() {
+        // $(this).slideDown('slow', function() {
+        //     alert("done!");
+        // });
+        //alert("It works!")
+    })
+});
