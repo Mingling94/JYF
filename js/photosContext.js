40e5204a93193bf5cc4639e0d657dbcddef6b2cd
Ming Luo <mcluo1994@gmail.com>
2015-10-04 06:54:19 -0400

    Saving state
    before experimenting with asynchronous methods

--- js/photosContext.js
+++ js/photosContext.js
@@ -7,15 +7,38 @@ function getStyle(elem, styleProp) {
 	}
     return value;
 }
-    
+
+ /**
+ * Convert an image 
+ * to a base64 url
+ * @param  {String}   url         
+ * @param  {Function} callback    
+ * @param  {String}   [outputFormat=image/png]           
+ */
+function convertImgToBase64URL(url, callback, outputFormat){
+    var img = new Image();
+    img.crossOrigin = 'Anonymous';
+    img.onload = function(){
+        var canvas = document.createElement('CANVAS'),
+        ctx = canvas.getContext('2d'), dataURL;
+        canvas.height = this.height;
+        canvas.width = this.width;
+        ctx.drawImage(this, 0, 0);
+        dataURL = canvas.toDataURL(outputFormat);
+        callback(dataURL);
+        canvas = null; 
+    };
+    img.src = url;
+}   
+
 // Get statuses as they are currently
 function getImages(callback) {
 	// Process array of .uiMediaThumbImg elements
     var elements = document.getElementsByClassName('uiMediaThumbImg');
-	var imgs = [];
 
-    // iterate over the elements
-    for (var i = 0; elements[i] ;i++) {
+    // iterate over the elements, getting png urls
+	var imgs = [];
+    for (var i = 0; elements[i];i++) {
 		var temp = elements[i];
         // get the background-image style property
         temp = getStyle(temp, 'background-image');
@@ -25,7 +48,15 @@ function getImages(callback) {
 
 		imgs[i] = temp;
     }
-	console.log(imgs);
+
+    // iterate over the elements, converting png to base64
+	var photos = $.map(imgs, function(imgurl, index) {
+		convertImgToBase64URL(imgurl, function(base64Img){
+		    // Base64DataURL
+			photos[index] = base64Img;
+		});
+	});
+	console.log(photos);
 
 	// TODO: Convert images
 	//getSentiments_Img(results, callback);
