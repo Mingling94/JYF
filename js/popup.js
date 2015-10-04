0c6ad225c7206571a911f566d073ba3f063bfdfb
Ming Luo <mcluo1994@gmail.com>
2015-10-04 03:47:49 -0400

    data shows up

--- js/popup.js
+++ js/popup.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	// Send message to the current tab
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		chrome.tabs.sendMessage(tabs[0].id, "getStatuses", function(response) {
-			alert(response);
+			graphStatusData(response);
 		});
 	});
 });
