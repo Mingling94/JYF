a2ed17fb14f23675a0058f370f3b0117626e1808
lunete <claud@openmailbox.org>
2015-10-03 20:35:15 -0400

    fixed some formating

--- js/indico.js
+++ js/indico.js
@@ -2,14 +2,15 @@ var statuses = ["Omg", "Becky", "No way"];
 var apikey = "368f9df404609aed0e36f71d89e5b992";
 
 $.ajax({
-  url: "https://apiv2.indico.io/sentiment/batch?key=" + apikey,
-  type: "POST", 
-  data: JSON.stringify({"data": statuses}), 
-  success: function (data) {
-    console.log(data);
-  }, error: function(err) {
-    console.log(err);
-  }
-})
+    url: "https://apiv2.indico.io/sentiment/batch?key=" + apikey,
+    type: "POST",
+    data: JSON.stringify({"data": statuses}),
+    success: function (data) {
+        console.log(data); //indent
+    }, error: function(err) {
+        console.log(err); //indent
+    }
+});
 
 console.log(document.getElementsByTagName("p"));
+// should find child of .fbTimelineCapsule, or ._5
