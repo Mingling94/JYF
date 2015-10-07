2e9628d923739ce073a10a11e4656fc3abfdc052
Ming Luo <mcluo1994@gmail.com>
2015-10-07 16:16:34 -0400

    Cleaned up
    - Got rid of unused files
    - Made new folders lib, assets to hold appropriate files and unclutter
    the root directory
    - Linted through files to replace whitespace line-beginnings with tabs

--- js/popup.js
+++ js/popup.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
 	// Send message to the current tab
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    	// since only one tab should be active and in the current window at once
-    	// the return variable should only have one entry
-    	var taburl = tabs[0].url;
-    	var tabid = tabs[0].id;
+		// since only one tab should be active and in the current window at once
+		// the return variable should only have one entry
+		var taburl = tabs[0].url;
+		var tabid = tabs[0].id;
 		var msg;
 		if (taburl.indexOf("photos") > -1) {
 			var port = chrome.tabs.connect(tabid, {name: "photos"});
